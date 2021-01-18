3b5520d8d81edc3fb533749268736f0fcad12ce7
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2021-01-18 12:57:26 +0100

    Oppdaterer avhengigheter (#22)
    * Oppdaterer dusseldorf-ktor og kotlin jvm
    
    * Nedgraderer kotlin jvm til 1.4.0. Får følgende feil i miljø "java.lang.IllegalStateException: Acquiring blocking primitives on this dispatcher is not allowed. Consider using async channel or doing withContext(Dispatchers.IO) { call.receive<InputStream>().use { ... } } instead."
    Mulig knyttet til https://youtrack.jetbrains.com/issue/KTOR-1286
    
    * deploy
    
    * Oppdaterer dusseldorf-ktor, gradle, kotlin.
    Flytter rekkefølgen på repositories.
    LEgger til withContext(Dispatchers.IO) ved bruk av receiveStream
    
    * Fjerner dependabot-task.
    Oppdaterer til implementation og testImplementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val dusseldorfKtorVersion = "1.3.2.9c88b78"
+val dusseldorfKtorVersion = "1.5.0.8b77f1d"
 val ktorVersion = ext.get("ktorVersion").toString()
 val kafkaEmbeddedEnvVersion = "2.2.0"
 val kafkaVersion = "2.3.0" // Alligned med version fra kafka-embedded-env
@@ -12,19 +12,16 @@ val mainClass = "no.nav.helse.OmsorgspengerMottakKt"
 
 
 plugins {
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.4.21"
     id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/9c88b788e57939fb6e3d18650393a7e9fb65960f/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/8b77f1d53e98bad7c081c3463871021bb8edc51f/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 repositories {
-    maven("http://packages.confluent.io/maven/")
-    jcenter()
     mavenLocal()
-    mavenCentral()
 
     maven {
         name = "GitHubPackages"
@@ -34,33 +31,37 @@ repositories {
             password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
         }
     }
+
+    mavenCentral()
+    jcenter()
+    maven("http://packages.confluent.io/maven/")
 }
 
 
 dependencies {
     // Server
-    compile("no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
 
     // Client
-    compile("no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
 
     // Kafka
-    compile("org.apache.kafka:kafka-clients:$kafkaVersion")
-    compile("no.nav:brukernotifikasjon-schemas:$brukernotifikasjonSchemaVersion")
-    compile("io.confluent:kafka-avro-serializer:$confluentVersion")
+    implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
+    implementation("no.nav:brukernotifikasjon-schemas:$brukernotifikasjonSchemaVersion")
+    implementation("io.confluent:kafka-avro-serializer:$confluentVersion")
 
     // Test
-    testCompile("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
-    testCompile("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
-    testCompile("io.ktor:ktor-server-test-host:$ktorVersion") {
+    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
+    testImplementation("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
     }
-    testCompile("org.skyscreamer:jsonassert:1.5.0")
+    testImplementation("org.skyscreamer:jsonassert:1.5.0")
 }
 
 java {
@@ -70,7 +71,7 @@ java {
 
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 
@@ -87,21 +88,5 @@ tasks.withType<ShadowJar> {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "5.6"
-}
-
-tasks.register("createDependabotFile") {
-    doLast {
-        mkdir("$projectDir/dependabot")
-        val file = File("$projectDir/dependabot/build.gradle")
-        file.writeText( "// Do not edit manually! This file was created by the 'createDependabotFile' task defined in the root build.gradle.kts file.\n")
-        file.appendText("dependencies {\n")
-        project.configurations.getByName("runtimeClasspath").allDependencies
-            .filter { it.group != rootProject.name && it.version != null }
-            .forEach { file.appendText("    compile '${it.group}:${it.name}:${it.version}'\n") }
-        project.configurations.getByName("testRuntimeClasspath").allDependencies
-            .filter { it.group != rootProject.name && it.version != null }
-            .forEach { file.appendText("    testCompile '${it.group}:${it.name}:${it.version}'\n") }
-        file.appendText("}\n")
-    }
+    gradleVersion = "6.7.1"
 }
